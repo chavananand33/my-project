d202c99bac0b2b186b139f3d6394ee62f306f02d
Anand Chavan <chavananand33@gmail.com>
2026-03-06 14:57:41 +0530

    fix database table creation

--- app.py
+++ app.py
@@ -222,7 +222,18 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object("config.Config")
+
+db = SQLAlchemy(app)
+
+class Counter(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    visits = db.Column(db.Integer, default=0)
+
+
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
-
-
