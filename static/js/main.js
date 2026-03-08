af089559a4daca3a4795b17858b258bac06d9bb6
Anand Chavan <chavananand33@gmail.com>
2026-03-08 22:07:25 +0530

    fix CSS, MAIN.JS and HTML5

--- static/js/main.js
+++ static/js/main.js
@@ -204,3 +204,16 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
 });
+
+const archBoxes = document.querySelectorAll('.arch-box');
+
+archBoxes.forEach((box, index) => {
+  box.style.opacity = "0";
+  box.style.transform = "translateY(30px)";
+
+  setTimeout(() => {
+    box.style.transition = "all 0.5s ease";
+    box.style.opacity = "1";
+    box.style.transform = "translateY(0)";
+  }, index * 200);
+});
